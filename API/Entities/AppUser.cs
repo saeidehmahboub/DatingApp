ebc6b9a9a1ccbae102656470710151c1765f8d33
Saeideh Mahboub <saeideh.mahboub@gmail.com>
2025-11-28 07:41:23 +0100

    Entity Framework relationships

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,4 +8,7 @@ public class AppUser
     public string? ImageUrl { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
+
+    // Navigation property
+    public Member Member { get; set; } = null!;
 }
